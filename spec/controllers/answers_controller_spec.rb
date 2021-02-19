f940d6b6eda2fd1c3742edb53d5e4c30539b67c1
Lexxicus <alexxxicus@gmail.com>
2021-02-19 21:05:48 +0500

    revisions

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe AnswersController, type: :controller do
         expect do
           post :create,
                params: { question_id: question, answer: attributes_for(:answer) }
-        end.to change(Answer, :count).by(1)
+        end.to change(question.answers, :count).by(1)
       end
       it 'redirects to show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
