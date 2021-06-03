9c376190069227fc80bc7980227ef69023045c24
Lexxicus <alexxxicus@gmail.com>
2021-06-04 00:27:44 +0500

    question search fix

--- app/indices/question_index.rb
+++ app/indices/question_index.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 ThinkingSphinx::Index.define :question, with: :active_record do
-  # fields
-  idexes title, sortable: true
+  indexes title, sortable: true
   indexes body
-  indexes user.email, as: :author, sortable: true
 
-  # attributes
-  has user_id, created_at, updated_at
+  has created_at, updated_at
 end
