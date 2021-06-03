5cdfb5f749d7be026c112c5f40144ef6dbea2df2
Lexxicus <alexxxicus@gmail.com>
2021-06-03 23:43:18 +0500

    repair mail v4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_cable.allowed_request_origins = ["http://31.184.254.70"]
+  config.action_cable.allowed_request_origins = ["31.184.254.70"]
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -68,11 +68,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'http://31.184.254.70/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
+    address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'apikey',
-    password: Rails.application.credentials[Rails.env.to_sym][:sendgrid][:api_key],
-    domain: 'http://31.184.254.70/',
+    user_name: Rails.application.credentials[Rails.env.to_sym][:gmail][:smtp_username],
+    password: Rails.application.credentials[Rails.env.to_sym][:gmail][:smtp_password],
     authentication: 'plain',
     enable_starttls_auto: true
   }
